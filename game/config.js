aa6a1a0d1aff90c413a26a08aca436e60927a359
midesweb <malvarez@desarrolloweb.com>
2020-10-25 10:36:50 +0100

    escalado

--- game/config.js
+++ game/config.js
@@ -4,6 +4,10 @@ export const createConfig = (scenes) => {
     width: 800,
     height: 500,
     scene: scenes,
+   scale: {
+     mode: Phaser.Scale.FIT,
+     autoCenter: Phaser.Scale.CENTER_BOTH
+   },
     physics: {
       default: 'arcade',
       arcade: {
@@ -11,5 +15,9 @@ export const createConfig = (scenes) => {
         debug: false
       }
     },
+   scale: {
+     mode: Phaser.Scale.FIT,
+     autoCenter: Phaser.Scale.CENTER_BOTH
+   },
   }
 }
